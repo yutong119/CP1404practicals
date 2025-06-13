f9f48d346a6027c3b0b4a4bfd908dbff87826fea
yutong119 <yutong.ji@my.jcu.edu.au>
2025-06-13 12:30:20 +0800

    answer the final questions

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -46,4 +46,5 @@ print(numbers_greater_than_9)
 # TODO: (more advanced) use a list comprehension and the join string method
 # to create a string (not list) of the last names for those full names longer than 11 characters
 # the result should be: 'Harlem, Hendrix, Lovelace'
-# last_name =
+last_names = ", ".join([name.split()[1] for name in full_names if len(name) > 11])
+print(last_names)
