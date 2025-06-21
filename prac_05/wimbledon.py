2d7180e164937384cbae1ae19330e380386a7e0a
yutong119 <yutong.ji@my.jcu.edu.au>
2025-06-21 16:36:59 +0800

    docs: Use set to collect the collection of countries
    Code:
    def get_countries(data):

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -11,3 +11,21 @@ def read_data():
             data.append(line.strip().split(','))
     return data
 
+def count_champions(data):
+    """Count the number of times the champion has won awards"""
+    champion_counts = {}
+    for row in data[1:]:
+        champion = row[2]
+        if champion in champion_counts:
+            champion_counts[champion] += 1
+        else:
+            champion_counts[champion] = 1
+    return champion_counts
+
+def get_countries(data):
+    """The collection of countries to which the champion belongs"""
+    countries = set()
+    for row in data[1:]:
+        countries.add(row[1])
+    return sorted(countries)
+
