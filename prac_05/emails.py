94b6c240e6381fefa2041a01c0ee56bcf0a646da
yutong119 <yutong.ji@my.jcu.edu.au>
2025-06-21 15:10:44 +0800

    fix: In the else branch, the user_name entered by the user is stored in the dictionary
    Relevant code：
    email_to_name[email] = user_name

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -7,10 +7,12 @@ email_to_name = {}
 email = input("Email: ")
 while email != "":
     guessed_name = email.split('@')[0]
+
     choice = input(f"Is your name {guessed_name}? (Y/n)")
     if choice == "Y" or choice == "":
         email_to_name[email] = guessed_name
     else:
         user_name = input("Name: ")
-        email_to_name[email] = guessed_name
+        email_to_name[email] = user_name
+
 print(email_to_name)
