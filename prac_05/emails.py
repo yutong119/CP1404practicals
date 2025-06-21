0ec3491884060b2688c582dd833fdc222a82de94
yutong119 <yutong.ji@my.jcu.edu.au>
2025-06-21 15:01:04 +0800

    refactor: change user_name to guessed_name, to better distinguishing variables

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -6,7 +6,7 @@ Actual:    minutes
 email_to_name = {}
 email = input("Email: ")
 while email != "":
-    user_name = email.split('@')[0]
+    guessed_name = email.split('@')[0]
     choice = input(f"Is your name {user_name}? (Y/n)")
     if choice == "Y" or choice == "":
         email = input("Email: ")
