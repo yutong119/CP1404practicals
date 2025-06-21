17f766aa177e3388e2144a5e053b9b77ea9e49a5
yutong119 <yutong.ji@my.jcu.edu.au>
2025-06-21 15:17:41 +0800

    fix: Update the loop conditions
    Relevant code：
    email = input("Email: ")

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -8,12 +8,14 @@ email = input("Email: ")
 while email != "":
     guessed_name = email.split('@')[0]
 
-    choice = input(f"Is your name {guessed_name}? (Y/n)")
-    if choice == "Y" or choice == "":
+    choice = input(f"Is your name {guessed_name}? (Y/n)").lower()
+    if choice == 'y' or choice == '':
         email_to_name[email] = guessed_name
     else:
         user_name = input("Name: ")
         email_to_name[email] = user_name
+
+    email = input("Email: ")
 
 for email, name in email_to_name.items():
     print(f"{name} ({email})")
