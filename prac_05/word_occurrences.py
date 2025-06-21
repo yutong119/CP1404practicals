98b125b90c1fb67aa1743cfaa35eb52ebfb22177
yutong119 <yutong.ji@my.jcu.edu.au>
2025-06-21 14:20:50 +0800

    count the occurrences of words in a string

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,23 @@
+"""
+Word Occurrences
+Estimate: 25 minutes
+Actual:    minutes
+"""
+text = input("Text: ")
+words = text.split()
+
+words_counts = {}
+for word in words:
+    if word in words_counts:
+        words_counts[word] += 1
+    else:
+        words_counts[word] = 1
+
+max_length = 0
+for word in words_counts:
+    if len(word) > max_length:
+        max_length = len(word)
+
+for word in sorted(words_counts.keys()):
+    print(f"{word:{max_length}} = {words_counts[word]}")
+
