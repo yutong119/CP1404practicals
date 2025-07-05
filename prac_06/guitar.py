a5b655ce798fd460dfa776d27422632895242893
yutong119 <yutong.ji@my.jcu.edu.au>
2025-07-05 10:38:23 +0800

    docs: Use __init__ function to store the fields of name, year, coat

---
+++ prac_06/guitar.py
@@ -0,0 +1,10 @@
+"""
+Estimate time: 20min
+Actual time:
+"""
+class Guitar:
+    def __init__(self, name="", year="", cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
