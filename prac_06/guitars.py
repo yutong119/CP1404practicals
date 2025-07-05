e24b7bcc74ea4ca2d7bea3ebdce05a1226690172
yutong119 <yutong.ji@my.jcu.edu.au>
2025-07-05 11:55:30 +0800

    Docs: Add the vintage_str use if, else statement

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -18,7 +18,8 @@ while name != "":
 print("These are my guitars:")
 for i, guitar in enumerate(guitars, 1):
     # do something with i (the index) and guitar (the element)
-
+    vintage = guitar.is_vintage()
+    vintage_str = " (vintage)" if vintage else ""
     print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
 
 
