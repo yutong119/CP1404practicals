d2ede0f51b4498286264f6a0b942976fd66b42eb
yutong119 <yutong.ji@my.jcu.edu.au>
2025-07-05 11:02:40 +0800

    docs: Create object of 2 guitars

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,6 @@
+"""Test that the last two methods(get_age, is_vintage) work as expected."""
+from prac_06.guitar import Guitar
+
+gibson = Guitar("Gibson L-5 CES", 1925, 100)
+another_guitar = Guitar("Another Guitar", 2013, 900)
+
