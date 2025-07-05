97d33a547cffad0546f5baebda83c81cfa6d72c7
yutong119 <yutong.ji@my.jcu.edu.au>
2025-07-05 11:07:20 +0800

    Docs: Test the get age method

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -4,3 +4,12 @@ from prac_06.guitar import Guitar
 gibson = Guitar("Gibson L-5 CES", 1925, 100)
 another_guitar = Guitar("Another Guitar", 2013, 900)
 
+# Test the get age method
+expected_age_gibson = 100
+actual_age_gibson = gibson.get_age()
+print(f"Another Guitar get_age() - Expected {expected_age_gibson}. Got {actual_age_gibson}")
+
+expected_age_another = 9
+actual_age_another = another_guitar.get_age()
+print(f"Another Guitar get_age() - Expected {expected_age_another}. Got {actual_age_another}")
+
