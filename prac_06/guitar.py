a042f2f3e996b7d4ac12c05a6fdfb1675ce38d49
yutong119 <yutong.ji@my.jcu.edu.au>
2025-07-05 10:40:28 +0800

    docs: Add __str__ function to return the string

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -3,8 +3,14 @@ Estimate time: 20min
 Actual time:
 """
 class Guitar:
-    def __init__(self, name="", year="", cost=0):
+    def __init__(self, name="", year=0, cost=0):
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        return f"{self.name} ({self.year}) : ${self.cost}"
+
+    def get_age
+
 
