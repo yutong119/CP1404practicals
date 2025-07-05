a70f6ceeae0878bfa5b8b2fb2c24f2dfa2855ca9
yutong119 <yutong.ji@my.jcu.edu.au>
2025-07-05 11:57:22 +0800

    Docs: Update the practice time

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,6 +1,6 @@
 """
 Estimate time: 30min
-Actual time:
+Actual time: 20min
 """
 from prac_06.guitar import Guitar
 
@@ -9,7 +9,7 @@ guitars = []
 name = input("Name: ")
 
 while name != "":
-    year = input("Year: ")
+    year = int(input("Year: "))
     cost = float(input("Cost: "))
     guitars.append(Guitar(name, year, cost))
     print(f"{Guitar(name, year, cost)} added.")
@@ -19,7 +19,7 @@ print("These are my guitars:")
 for i, guitar in enumerate(guitars, 1):
     # do something with i (the index) and guitar (the element)
     vintage = guitar.is_vintage()
-    vintage_str = " (vintage)" if vintage else ""
+    vintage_string = " (vintage)" if vintage else ""
     print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
 
 
