9bc1dd41394aa1bbcf8fffdb4c2b46e4c66d7819
yutong119 <yutong.ji@my.jcu.edu.au>
2025-07-05 10:00:14 +0800

    docs: Create __init__ function and set the parameters of name, typing, reflection, year

---
+++ prac_06/programming_language.py
@@ -0,0 +1,10 @@
+class ProgrammingLanguage:
+    """ """
+    def __init__(self, name="", typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self. year = year
+
+    def is_dynamic(self):
+
