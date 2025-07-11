e89d2b4a5fcf115a3be5b4a1e2e126c214411725
yutong119 <yutong.ji@my.jcu.edu.au>
2025-07-11 17:56:36 +0800

    Docs: Use for loop to read data and append them to the list

--- prac_07/guitar_reader.py
+++ prac_07/guitar_reader.py
@@ -5,5 +5,8 @@ def main():
     in_file = open('guitars.csv', 'r')
     for line in in_file:
         parts = line.strip().split(',')
-
+        year = int(parts[1])
+        cost = float(parts[2])
+        guitar = Guitar(parts[0], year, cost)
+        guitars.append(guitar)
     in_file.close()
