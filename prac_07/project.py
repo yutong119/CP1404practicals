21e9ba0ab7d25a023869a0e4c66d496cec233981
yutong119 <yutong.ji@my.jcu.edu.au>
2025-07-13 19:29:26 +0800

    Docs: Create __str__ method

--- prac_07/project.py
+++ prac_07/project.py
@@ -12,4 +12,17 @@ class Project:
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
 
+    def __lt__(self, other):
+        """Support sorting by priority"""
+        return self.priority < other.priority
+
+    def is_completed(self):
+        """Determine whether the project has been completed"""
+        return self.completion_percentage == 100
+
+    def __str__(self):
+        """Return string"""
+        return (f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, "
+                f"priority {self.priority}, estimate: ${self.cost_estimate:.2f}, "
+                f"completion: {self.completion_percentage}%")
 
