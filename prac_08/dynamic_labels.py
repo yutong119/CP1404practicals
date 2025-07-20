56a40a7c95119669b6e8f11f73998c6332c3746c
yutong119 <yutong.ji@my.jcu.edu.au>
2025-07-20 14:31:24 +0800

    Docs: Use for loop to generates a separate Label for each name

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -10,3 +10,12 @@ class DynamicLabelsApp(App):
         super().__init__(**kwargs)
         self.names = ["Alice", "Bob", "Charlie", "Diana"]
 
+    def build(self):
+        root_layout = self.root.ids.main
+
+        for name in self.names:
+            new_label = Label(text=name)
+            root_layout.add_widget(new_label)
+
+        return self.root
+
