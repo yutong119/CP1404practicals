c206d07711f746f0ed33dbcc778409b401fbbc09
yutong119 <yutong.ji@my.jcu.edu.au>
2025-07-27 19:51:07 +0800

    Docs: Add main function

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,30 @@
+from prac_09.silver_service_taxi import SilverServiceTaxi
+from prac_09.taxi import Taxi
+MENU = """
+q)uit, c)hoose taxi, d)rive"""
+
+def main():
+    taxis = [Taxi("Prius", 100),
+             SilverServiceTaxi("Limo", 100, 2),
+             SilverServiceTaxi("Hummer", 200, 4)]
+
+    print("Let's drive!")
+    print(MENU)
+    choice = get_valid_choice()
+    while choice != 'q':
+        if choice == 'c':
+            print(f"Taxis available:")
+
+        elif choice == 'd':
+
+
+
+def get_valid_choice():
+    choice = input(">>>")
+    if choice not in ['q', 'c', 'd']:
+        print("Invalid choice")
+    else:
+        return choice
+
+
+
