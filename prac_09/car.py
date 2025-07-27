84eadc32d4717f24ccf508df96a12304ed8bebf1
yutong119 <yutong.ji@my.jcu.edu.au>
2025-07-27 16:23:24 +0800

    Fix： Makesure the type of fuel is int
    Change the index of name and fuel

--- prac_09/car.py
+++ prac_09/car.py
@@ -2,12 +2,12 @@
 class Car:
     """Represent a Car object."""
     #6. Now add a name field to the Car class (in car.py), and adjust the __init__ and __str__ methods to set and display this respectively. Make the str method return the car's name instead of just "Car".
-    def __init__(self, fuel=0, name="Car"):
+    def __init__(self, name="Car", fuel=0):
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
         """
-        self.fuel = fuel
+        self.fuel = int(fuel)
         self._odometer = 0
         self.name = name
 
@@ -17,7 +17,7 @@ class Car:
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
-        self.fuel += amount
+        self.fuel += int(amount)
 
     def drive(self, distance):
         """Drive the car a given distance.
