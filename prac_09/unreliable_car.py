9b48e8023fa5ebb2a2590bb6bdff70c879cdb710
yutong119 <yutong.ji@my.jcu.edu.au>
2025-07-27 17:32:54 +0800

    Fix: Add docstring to functions

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -2,12 +2,15 @@ from prac_09.car import Car
 import random
 
 class UnreliableCar(Car):
-    """"""
+    """Assess whether one can drive a car through risk"""
     def __init__(self, name, fuel, reliability):
+        """Initialize name, fuel, reliability"""
         super().__init__(name, fuel)
         self.reliability = float(reliability)
 
     def drive(self, distance):
+        """Determine whether it can be driven by checking if the risk assessment value is less than reliability;
+        otherwise, return 0"""
         risk = random.randint(0, 100)
         if risk < self.reliability:
             return super().drive(distance)
