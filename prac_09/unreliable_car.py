49d291749807c6226d14f292fb503de2ed0cb88a
yutong119 <yutong.ji@my.jcu.edu.au>
2025-07-27 17:28:52 +0800

    Fix: Fix the red underline

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -3,14 +3,16 @@ import random
 
 class UnreliableCar(Car):
     """"""
-    def __init__(self, **kwargs, reliability: float):
-        super()__init__(**kwargs)
-        self.reliability = reliability
+    def __init__(self, name, fuel, reliability):
+        super().__init__(name, fuel)
+        self.reliability = float(reliability)
 
     def drive(self, distance):
         risk = random.randint(0, 100)
-        if risk < reliability:
+        if risk < self.reliability:
             return super().drive(distance)
+        else:
+            return 0
 
 
 
