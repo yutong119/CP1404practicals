c142a7742d26e76148a1d566ffec205c2d9dfb74
yutong119 <yutong.ji@my.jcu.edu.au>
2025-07-27 18:41:07 +0800

    Docs: Add __str__ call the str

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -3,6 +3,7 @@ from prac_09.taxi import Taxi
 class SilverServiceTaxi(Taxi):
     """"""
     flagfall = 4.50
+
     def __init__(self, kwargs, fanciness):
         """Initialize name, fuel, fanciness"""
         super().__init__(**kwargs)
@@ -11,3 +12,5 @@ class SilverServiceTaxi(Taxi):
         self.price_per_km = Taxi.price_per_km * fanciness
 
     def __str__(self):
+        return f"{super().__str__()} plus falgfall of ${self.flagfall}"
+
