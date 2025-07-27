5ff5e26ff27855ed07d94edee9e26acd8ddb84fb
yutong119 <yutong.ji@my.jcu.edu.au>
2025-07-27 18:35:08 +0800

    Docs: Initialize the objects

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,13 @@
+from prac_09.taxi import Taxi
+
+class SilverServiceTaxi(Taxi):
+    """"""
+    flagfall = 4.50
+    def __init__(self, kwargs, fanciness):
+        """Initialize name, fuel, fanciness"""
+        super().__init__(**kwargs)
+        self.fanciness = float(fanciness)
+        #set price_per_km
+        self.price_per_km = Taxi.price_per_km * fanciness
+
+    def __str__(self):
