f716be5faa3b8275856d612eb4be55687a292e0a
yutong119 <yutong.ji@my.jcu.edu.au>
2025-07-27 15:52:14 +0800

    Docs： drive the my_taxi and print the current fare

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -1,3 +1,6 @@
 from prac_09.taxi import Taxi
 
-my_taxi:Taxi("Prius 1", 100, 1.23)
+my_taxi = Taxi("Prius 1", 100, 1.23)
+my_taxi.drive(40)
+print(my_taxi)
+print(f"Current fare: ${my_taxi.get_fare():.2f}")
