72a4b5f22cdbd54c99e05c8f8e21aa2279674909
yutong119 <yutong.ji@my.jcu.edu.au>
2025-08-03 11:59:02 +0800

    Use the join method to concatenate a list containing n s with Spaces

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -9,7 +9,9 @@ from prac_06.car import Car
 
 def repeat_string(s, n):
     """Repeat string s, n times, with spaces in between."""
-    return s * n
+    if n == 1:
+        return s
+    return " ".join([s] * n )
 
 
 def is_long_word(word, length=5):
